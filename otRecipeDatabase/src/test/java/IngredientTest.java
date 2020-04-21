0c9a6cbec55be97346f75d9f3dae9bcd8b100929
jrhel <johannes.riska@helsinki.fi>
2020-04-21 21:00:43 +0300

    database diagram

---
+++ otRecipeDatabase/src/test/java/IngredientTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import main.domain.Ingredient;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author J
+ */
+public class IngredientTest {
+    
+    public IngredientTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void newIngredient() {
+        Ingredient blueberries = new Ingredient("Blueberries");
+        String name = blueberries.getName();
+        assertEquals("Blueberries", name);
+    }
+}
